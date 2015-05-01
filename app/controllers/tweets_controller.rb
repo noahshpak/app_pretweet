4f360662f028eba8084dc1a358773aff678f5aa0
Noah <noah@shpakee.com>
2015-05-01 19:52:22 -0400

    added webhook

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -189,7 +189,7 @@ class TweetsController < ApplicationController
       puts payload
       tweet_body = payload.fetch("data").fetch("content")
       puts tweet_body
-      approp = payload.fetch("results").fetch("judements").fetch("data").fetch("appropriate").fetch("agg")
+      approp = payload.fetch("results").fetch("judgements").fetch("data").fetch("appropriate").fetch("agg")
       puts approp
       if approp.eql? "yes"
         score = 1.0
